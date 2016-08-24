2d1a13122c256faba5069c1aa3bf3195c6d1c717
Roilan Hdez Valdes <roylanhv@gmail.com>
2016-08-24 17:25:04 -0300

    eliminando algumas linhas erradas

--- basic_function.R
+++ basic_function.R
@@ -2,12 +2,11 @@ basic_function <- function(col){
     col**2
 }
 
-<<<<<<< HEAD
+
 ## funcao 2
-=======
+
 
 sum.function <- function(a,b){
     soma = a+b
     return(soma)
 }
->>>>>>> c5d783ccbde66fae966b13a6b8ff15b5d62e93b6
